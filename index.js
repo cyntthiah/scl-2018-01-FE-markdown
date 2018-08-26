2c562626dac827f3849e4de9e72806088294a918
cynthia <cyntthiah@gmail.com>
2018-08-26 18:11:06 -0300

    new project created

---
+++ index.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+callweb("https://raw.githubusercontent.com/cyntthiah/scl-2018-01-FE-markdown/master/README.md");
+callweb("https://www.google.com");
+
+function callweb(web){
+    fetch(web)
+    .then(data=>{
+        console.log(data)
+    })
+    .catch(err=>{
+        console.log(err)
+    });
+
+    
+}
