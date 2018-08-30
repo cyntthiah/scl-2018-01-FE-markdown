cf7e878d853fa604391e211400d44da42ca10552
cynthia <cyntthiah@gmail.com>
2018-08-30 01:50:19 -0300

    counter

--- src/mdlinks.js
+++ src/mdlinks.js
@@ -1,72 +1,79 @@
 const Marked = require('marked');
+const fs = require('fs');
 const fetch = require('node-fetch');
-
-
-var fs = require('fs');
+var file = '../README.md';// ubicacion del archivo
+ 
+var text = fs.readFileSync(file,'utf8');
 var count = 0;
-var text = fs.readFileSync('/home/cynthia/Documentos/nodejs/scl-2018-01-FE-markdown/README.md','utf8');
-var arrayLinks = markdownLinkExtractor(text);
-for(var line in text)
+var validate = false;//arg validate 
+ 
+ 
+ 
+ 
+var lines = text.split('\n');
+for(var line of lines)//of trae contenido y no numeros
 {
-        console.log(line);
-}
-console.log(count);
-
-
-for(var i=0;i<arrayLinks.length;i++){
-    //console.log(arrayLinks[i].href);
-    let status = "";
-    fetch(arrayLinks[i].href)
-    .then(response=>{
-        if(response.ok == true)
+    let number = count++;//linea que cambia el numero
+    let textLine = markdownLinkExtractor(line);
+    if(typeof textLine[0] !== "undefined")
+    {
+       
+        if(validate == true)
         {
-             status =  "ok";
+            fetch(textLine[0].href).then(
+                (response)=>{
+                    if(response.ok = true)
+                    {
+                        console.log(file + ':' + numero + ' ' + response.url + ' is OK' );
+                    }
+                    else
+                    {
+                       
+                    }
+                }
+            ).catch((err)=>{
+                console.log(file + ':' + numero + ' ' + textLine[0].href + 'is Broken' );
+            })
         }
-        else
-        {
-            status = "broken";
+        else{
+            console.log(file + ':' + count + ' ' + textLine[0].href);
         }
-        console.log(response.url + ' is ' + status);
-        
-    }) 
-    .catch(err=>{
-        console.log(err);
-    });
-    
+    }
+    else
+    {
+       
+    }
 }
-
-
+console.log(count);//imprime las lineas totales del texto
+ 
+ 
+ 
+ 
 function markdownLinkExtractor(markdown) {
-    const links = [];
-  
-    const renderer = new Marked.Renderer();
-  
-    
-    const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
-  
-    Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
-    Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
-    Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
-  
-    renderer.link = function(href, title, text) {
+  const links = [];
+  const renderer = new Marked.Renderer();
+  const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
+ 
+  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
+ 
+  renderer.link = function(href, title, text) {
+    links.push({
+      href: href,
+      text: text,
+      title: title,
+    });
+  };
+  renderer.image = function(href, title, text) {
+      href = href.replace(/ =\d*%?x\d*%?$/, '');
       links.push({
         href: href,
         text: text,
         title: title,
       });
-    };
-    renderer.image = function(href, title, text) {
-        
-        href = href.replace(/ =\d*%?x\d*%?$/, '');
-        links.push({
-          href: href,
-          text: text,
-          title: title,
-        });
-    };
-    Marked(markdown, {renderer: renderer});
-  
-    return links;
   };
- console.log(`Current directory: ${process.cwd('./../README.md')}`);
-
+  Marked(markdown, {renderer: renderer});
+ 
+  return links;
+};
